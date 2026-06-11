8840a6be08e5cc28f3a9a3994ccbadef68f61461
jannatulferdou <jannatulfer2005@gmail.com>
2026-06-11 16:23:51 +0600

    added some updates

---
+++ frontend/app/index.tsx
@@ -0,0 +1,28 @@
+
+import useAuth from "@/hooks/useAuth";
+import { Redirect } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
+
+export default function Index() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  if (user) {
+    return <Redirect href="/(home)" />;
+  }
+
+  return <Redirect href="/(auth)/splash" />;
+}
